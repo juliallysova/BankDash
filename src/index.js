024d1b18d679830f8b5ad2ff26e7db9b37aa3c81
juliallysova <julik1001@yandex.ru>
2024-11-10 10:30:13 +0300

    load to GIT

--- src/index.js
+++ src/index.js
@@ -3,11 +3,76 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './Root';
+import ErrorPage from './error-page';
+import Contact from './contact';
+import Accounts from './pages/accounts/accounts';
+import CreditCards from './pages/creditCards/creditCards';
+import Dashboard from './pages/dashboard/dashboard';
+import Investments from './pages/investments/investments';
+import Loans from './pages/loans/loans';
+import MyPrivileges from './pages/myPrivileges/myPrivileges';
+import Services from './pages/services/services';
+import Settings from './pages/settings/settings';
+import Transactions from './pages/transactions/transactions';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Root/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "accounts",
+        element: <Accounts />,
+      },
+      {
+        path: "creditCards",
+        element: <CreditCards />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "investments",
+        element: <Investments/>,
+      },
+      {
+        path: "loans",
+        element: <Loans />,
+      },
+      {
+        path: "myPrivileges",
+        element: <MyPrivileges />,
+      },
+      {
+        path: "services",
+        element: <Services />,
+      },
+      {
+        path: "settings",
+        element: <Settings />,
+      },
+      {
+        path: "transactions",
+        element: <Transactions />,
+      },
+    ],
+  },
+  // {
+  //   path: "contacts/:contactId",
+  //   element: <Contact />,
+  // }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
